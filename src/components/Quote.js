3279499c520328b50295f43796f42158dd2a8227
Elena-Weber <crochetmiracles@gmail.com>
2021-10-28 19:55:59 -0400

    add error handling

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,23 +2,29 @@ import React from 'react';
 
 const Quote = (quotes) => {
 
-    return (
-        <div className="quotes">
-            { quotes.quotes.map((q, id) => (
-                <div className="quote" key={id} >
-                    <h3 className="quoteContent"> {q.props.quotes.content} </h3>
-                    <div className="details">
-                        <p><span className="quoteDetails">Source:</span> {q.props.quotes.author} </p>
-                        <div className="quoteTags">
-                            {q.props.quotes.tags.map((tag, id) =>
-                                <li key={id}> {tag} </li>
-                            )}
+    if (quotes.quotes.length === 0) {
+        return (
+            <h3>Sorry, there is nothing to display. Please search for another word or make sure the spelling is correct.</h3>
+        )
+    } else {
+        return (
+            <div className="quotes">
+                { quotes.quotes.map((q, id) => (
+                    <div className="quote" key={id} >
+                        <h3 className="quoteContent"> {q.props.quotes.content} </h3>
+                        <div className="details">
+                            <p><span className="quoteDetails">Source:</span> {q.props.quotes.author} </p>
+                            <div className="quoteTags">
+                                {q.props.quotes.tags.map((tag, id) =>
+                                    <li key={id}> {tag} </li>
+                                )}
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
-        </div>
-    )
+                ))}
+            </div>
+        )
+    }
 }
 
 export default Quote;
